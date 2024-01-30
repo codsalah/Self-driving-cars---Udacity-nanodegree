5d12de4306b99681bfb37218c7778bd94311a248
salah mohamed <salahhamada63@gmail.com>
2024-01-30 07:34:50 +0200

    task1p

---
+++ intro-to-self-driving/robot-localization/task1.py
@@ -0,0 +1,19 @@
+#Write code that outputs p after multiplying each entry 
+#by pHit or pMiss at the appropriate places. Remember that
+#the red cells 1 and 2 are hits and the other green cells
+#are misses.
+
+
+p=[0.2,0.2,0.2,0.2,0.2]
+pHit = 0.6
+pMiss = 0.2
+
+# sol
+for i in range(len(p)):
+    if i + 1 in [1, 2]:  
+        p[i] *= pHit
+    else:
+        p[i] *= pMiss
+
+# Output the updated list
+print(p)
